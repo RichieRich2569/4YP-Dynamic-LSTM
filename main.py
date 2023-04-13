c172f77539a8c6f69db24c99b9419802c24c0236
RichieRich2569 <rm967@cam.ac.uk>
2023-04-13 15:51:01 +0100

    Added circular trajectory, fixed slow3 to match input limits.

--- main.py
+++ main.py
@@ -33,41 +33,41 @@ def main():
     # Obtain output via preset input
 
     # Create sinusoid - f
-    # f = 0.2 # in Hz
-    # t = np.linspace(0,10,30*30)
-    # xsin = np.zeros((len(t),6))
-    # xsin[:,0] = 2*np.random.rand(1)*(np.sin(t*2*np.pi*f+np.random.rand(1)*np.pi)+3)
-    # xsin[:,1] = np.random.rand(1)*3
-    # xsin[:,2] = 2*np.random.rand(1)*(np.sin(t*2*np.pi*f+np.random.rand(1)*np.pi)+5)
-    # xsin[:,3] = np.random.rand(1)*3
-    # xsin[:,4] = 2*np.random.rand(1)*(np.sin(t*2*np.pi*f+np.random.rand(1)*np.pi)+3.2)
-    # xsin[:,5] = np.random.rand(1)*3
+    f = 0.2 # in Hz
+    t = np.linspace(0,10,30*30)
+    xsin = np.zeros((len(t),6))
+    xsin[:,0] = 1.5*np.random.rand(1)*(np.sin(t*2*np.pi*f+np.random.rand(1)*np.pi)+1.0)
+    xsin[:,1] = np.random.rand(1)*3
+    xsin[:,2] = 1.5*np.random.rand(1)*(np.sin(t*2*np.pi*f+np.random.rand(1)*np.pi)+1.0)
+    xsin[:,3] = np.random.rand(1)*3
+    xsin[:,4] = 1.5*np.random.rand(1)*(np.sin(t*2*np.pi*f+np.random.rand(1)*np.pi)+1.0)
+    xsin[:,5] = np.random.rand(1)*3
     
-    # x = torch.cat((torch.zeros(1,120,6),torch.Tensor(xsin).unsqueeze(0)),axis=1).to(device)
-    # out = model.get_output(lstm_model, x)
+    x = torch.cat((torch.zeros(1,120,6),torch.Tensor(xsin).unsqueeze(0)),axis=1).to(device)
+    out = model.get_output(lstm_model, x)
     
     # Obtain output via optimisation
     
     # Open image
-    y1 = Image.open('optimisation_img/im1.png').convert('L')
-    y1 = torch.from_numpy(np.array(y1)/255).to(device)
-    # Parameter definition and optimisation
-    T = 3 # 2 Seconds maximum time
-    t = [2]
-    y_des = [y1]
-    param = optimiser.optimise_inputs(lstm_model, y_des, t, T)
+    # y1 = Image.open('optimisation_img/im1.png').convert('L')
+    # y1 = torch.from_numpy(np.array(y1)/255).to(device)
+    # # Parameter definition and optimisation
+    # T = 3 # 2 Seconds maximum time
+    # t = [2]
+    # y_des = [y1]
+    # param = optimiser.optimise_inputs(lstm_model, y_des, t, T)
     
-    # Obtain X from optimised parameters, show output
-    N = int(SAMPLING_FREQ*T)
-    n = (SAMPLING_FREQ*torch.tensor(t)).type(torch.int)
-    x = np.reshape(param, (len(n),6))
-    X = optimiser.piecewise_generator(x, n, N)
-    out = model.get_output(lstm_model, X)
+    # # Obtain X from optimised parameters, show output
+    # N = int(SAMPLING_FREQ*T)
+    # n = (SAMPLING_FREQ*torch.tensor(t)).type(torch.int)
+    # x = np.reshape(param, (len(n),6))
+    # X = optimiser.piecewise_generator(x, n, N)
+    # out = model.get_output(lstm_model, X)
     
     # Play video from prediction
     #util.play_video(out[0,...])
-    util.save_video(out[0,...], 'control_data/videos/static_control1.avi')
-    util.save_data(X, 'control_data/static_control1.txt')
+    util.save_video(out[0,...], 'control_data/videos/slow3.avi')
+    util.save_data(x, 'control_data/slow3.txt')
     
     return 0
     
